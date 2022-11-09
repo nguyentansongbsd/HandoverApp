bda58c7415f9cf1675d7943cb4e826823f254603
phu <phamxuanphu98@gmail.com>
2022-11-09 17:12:13 +0700

    924

---
+++ HandoverApp.Android/Services/BaseUrl.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using HandoverApp.Droid.Services;
+using HandoverApp.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(BaseUrl))]
+namespace HandoverApp.Droid.Services
+{
+    public class BaseUrl : IBaseUrl
+    {
+        public string Get()
+        {
+            return "file:///android_asset/Content/";
+        }
+    }
+}
---
+++ HandoverApp/IServices/IBaseUrl.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HandoverApp.IServices
+{
+    public interface IBaseUrl { string Get(); }
+}
